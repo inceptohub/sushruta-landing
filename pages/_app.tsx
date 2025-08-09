d8c4742bf9abfcf24d447ece2cdfe3ef165fbd68
praveen-me <praveenkrsaini0@gmail.com>
2025-08-09 17:35:59 +0530

    feat: add Vercel Analytics to track website usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import Navbar from '../components/Navbar';
 import '@/styles/globals.css';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className="flex-grow">
         <Component {...pageProps} />
       </main>
+      <Analytics />
     </div>
   );
 }
